907bdb3a3791e15a206b637dcfc38a4bbb10c030
Anant <anantcode@gmail.com>
2021-09-23 06:28:29 +0530

    Converting content of xlf to JSON using xml-js

--- deployment.js
+++ deployment.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const convert = require("xml-js");
 
 function readFiles(dirname, onFileContent, onError) {
     fs.readdir(dirname, function (err, filenames) {
@@ -27,6 +28,9 @@ function handleError(err) {
     console.log(err);
 }
 
-function processContent(content) {}
+function processContent(content) {
+    var result1 = convert.xml2json(content, { compact: true, spaces: 4 });
+    console.log(result1);
+}
 
 readFiles("translated\\", forEachXliffFile, handleError);
